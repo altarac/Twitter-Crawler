b9c9da36ea3e920089d19775530059f1ae0dc419
Samuel Altarac <samuel.altarac@gmail.com>
2018-01-29 09:43:13 -0500

    twitter crawler
    gets science gifs from twitter

--- crawler.py
+++ crawler.py
@@ -1,63 +1,77 @@
-import tweepy
-from textblob import TextBlob
+# witter science gifs scraper
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+import time
+import pandas as pd
+from bs4 import BeautifulSoup as bs
 
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_token_secret = ''
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+# chromedriver = '/Users/samuelaltarac/Desktop/python programs/chromedriver'
+browser = webdriver.Chrome()
+browser.get('https://twitter.com/Learn_Things')
 
-class MyStreamListener(tweepy.StreamListener):
+time.sleep(1)
 
-    def on_status(self, status):
-        try:
-            analysis = TextBlob(status.text)
-            print(status.text)
-            print(analysis.sentiment)
-            datafile = open('/Users/samuelaltarac/Desktop/python programs/data.txt','a')
-            datafile.write(status.text)
-            datafile.write('\n')
-            datafile.write(str(analysis.sentiment))
-            datafile.write('\n')
-            datafile.close()
-            
-        except UnicodeEncodeError:
-            print('ERROR unicode')
+user = browser.find_element_by_class_name("email-input")
+user.send_keys('mraltarac')
 
-myStreamListener = MyStreamListener()
-myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+p = browser.find_element_by_name("session[password]")
+p.send_keys('**********')
 
-myStream.filter(track=['aapl', 'apple', 'tim cook', 'iphone stock', 'apple stock'])
+btn = browser.find_element_by_class_name("submit")
+btn.send_keys(Keys.RETURN)
 
 
 
 
+h = browser.find_element_by_tag_name('HTML')
+
+for i in range(0,100):
+    h.send_keys(Keys.END)
+    time.sleep(0.5)
+#     lis = soup.find_all('li', {"class": ['js-stream-item', 'stream-item', 'stream-item']})
+#     l = len(lis)
+#     if l == 810:
+#         break
+
+
+time.sleep(1)
+
+html_doc = browser.page_source
+soup = bs(html_doc, 'html.parser')
+
+lis = soup.find_all('li', {"class": ['js-stream-item', 'stream-item', 'stream-item']})
+
+l = len(lis)
+
+print('number of tweets should be: ' + str(l))
+
+tweets = soup.find_all("div", {"class": ['tweet', 'js-stream-tweet', 'js-actionable-tweet']}, limit=l)
+descriptions = soup.find_all("p", {"class": ['TweetTextSize', 'TweetTextSize--normal', 'js-tweet-text', 'tweet-text']}, limit=l)
+
+links = []
+descs = []
+# print(tweets)
+
+for t in tweets:
+    links.append('https://twitter.com' + str(t.attrs['data-permalink-path']))
+
+for d in descriptions:
+    descs.append(d.text)
 
 
 
+print(len(links))
+print(len(descs))
+
+
+browser.quit()
 
 
 
-"""
-public_tweets = api.search('learn')
+d = {'tags': 'twitter', 'descriptions' : descs, 'links': links}
 
-for tweet in public_tweets:
-    try:
-        print(tweet.text)
-    except UnicodeEncodeError:
-        print('UCS-2 codec cant encode characters in position 113-113: Non-BMP character not supported in Tk')
-"""
+df = pd.DataFrame(data=d)
 
-
-
-
-
-"""
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print (tweet.text)
-"""
-
+df.to_csv('science_GIFs4.csv')
